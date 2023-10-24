2eecb8185632d1a82d22b21b05272b520d484c1a
Paritosh Kumar Roy <paritosh.roy@mail.mcgill.ca>
2023-10-24 14:42:08 -0400

    Commit on 24 October 2023

--- ComparingApproximateMethods/NNNN_GLGC_Exp.sh
+++ ComparingApproximateMethods/NNNN_GLGC_Exp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --array=201-250  # 50 nodes runs this model independently
+#SBATCH --array=301-350  # 50 nodes runs this model independently
 #SBATCH --account=def-aschmidt  # replace this with your own account
 #SBATCH --ntasks=16              # number of processes
 #SBATCH --mem-per-cpu=16000M      # memory; default unit is megabytes
 #SBATCH --time=48:00:00         # time (HH:MM:SS)
-#SBATCH --output=/home/pkroy/projects/def-aschmidt/pkroy/ApproximateGLGC/%x-%j.out
+#SBATCH --output=/home/pkroy/projects/def-aschmidt/pkroy/ApproximateGLGC/ComparingApproximateMethods/%x-%j.out
 
 # Modules
 module load gcc/11.3.0
@@ -15,4 +15,4 @@ module load r/4.2.1
 # If all processes are allocated on the same node, NODESLIST contains : node1 node1 node1 node1
 # Cut the domain name and keep only the node name
 export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
-R -f GLGCMatern32NNNNAnalysis.R --args $SLURM_ARRAY_TASK_ID
+R -f NNNN_GLGC_Exp.R --args $SLURM_ARRAY_TASK_ID
---
+++ ComparingApproximateMethods/data_generation.R
@@ -0,0 +1,55 @@
+obj_prev <- ls()
+
+library(fields)
+library(Matrix)
+library(tidyverse)
+library(magrittr)
+library(tidybayes)
+library(sf)
+library(coda)
+library(nleqslv)
+
+coords <- unname(as.matrix(expand.grid(x = seq(-0.99, 0.99, length.out = 100), y = seq(-0.99, 0.99, length.out = 100))))
+nsite <- nrow(coords)
+beta <- c(5,-1,1)
+sigma1 <- 1
+sigma2 <- 1
+sigma1sq <- sigma1^2
+sigma2sq <- sigma2^2
+lscale1 <- sqrt(2)/2
+lscale2 <- sqrt(2)/2
+tau <- 0.5
+tausq <- tau^2
+gamma <- 1.5
+
+distMat <- fields::rdist(coords)
+
+SigmaX <- 1*0.25^abs(outer(1:2,1:2,'-'))
+set.seed(500) # seed for fixing the random effect and covariates
+X <- cbind(1,cbind(rnorm(n=nsite),rnorm(n=nsite)) %*% t(chol(SigmaX)))
+muX <- drop(X %*% beta)
+z1 <- drop(crossprod(chol(matern32(d = fields::rdist(coords), sigma = sigma1, lscale = lscale1) + diag(x=1e-9, nrow = nsite, ncol = nsite)), rnorm(nsite)))
+z2 <- drop(crossprod(chol(matern32(d = fields::rdist(coords), sigma = sigma2, lscale = lscale2) + diag(x=1e-9, nrow = nsite, ncol = nsite)), rnorm(nsite)))
+linpred <- muX +  gamma * exp(z1) + z2
+set.seed(NULL)
+
+set.seed(node*100) # seed for replicated datasets
+y <- rnorm(n = nsite, mean = linpred, sd = tau)
+set.seed(NULL)
+
+set.seed(1) # seed for selecting sampled locations
+nsize <- 1000
+idSampled <- sample.int(n = nsite, size = nsize, replace = FALSE)
+set.seed(NULL)
+
+#hist(y, nclass = 21)
+#hist(y[idSampled], nclass = 21)
+#plot(density(y[idSampled]))
+#boxplot(y[idSampled])
+
+obj_all <- ls()
+obj_keep <- c("idSampled", "y", "z1", "z2", "X", "beta", "sigma1", "sigma2", "lscale1", "lscale2", "tau", "coords", "gamma", "nsize")
+obj_drop <- obj_all[!obj_all %in% c(obj_prev, obj_keep)]
+rm(list = obj_drop)
+gc()
+
