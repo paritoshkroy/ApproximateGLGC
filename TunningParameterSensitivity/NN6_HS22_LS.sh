a65a174e0bc85a7e9d471d8d34d4c6f493b95fac
Paritosh Kumar Roy <paritosh.roy@mail.mcgill.ca>
2023-10-29 17:01:18 -0400

    Commit on 29 October 2023

--- TunningParameterSensitivity/NN6_HS22_LS.sh
+++ TunningParameterSensitivity/NN6_HS22_LS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=301-310  # 10 nodes runs this model independently
+#SBATCH --array=161-170  # 10 nodes runs this model independently
 #SBATCH --account=def-aschmidt  # replace this with your own account
 #SBATCH --ntasks=16              # number of processes
 #SBATCH --mem-per-cpu=16000M      # memory; default unit is megabytes
@@ -15,4 +15,4 @@ module load r/4.2.1
 # If all processes are allocated on the same node, NODESLIST contains : node1 node1 node1 node1
 # Cut the domain name and keep only the node name
 export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
-R -f NN15_HS22_LS.R --args $SLURM_ARRAY_TASK_ID
+R -f NN6_HS22_LS.R --args $SLURM_ARRAY_TASK_ID
