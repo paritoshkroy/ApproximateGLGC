6b82e6bb4569822fb0e3a77311f19b4bd2e12079
Paritosh Kumar Roy <paritosh.roy@mail.mcgill.ca>
2023-11-15 00:39:06 -0500

    Commit on 15 November 2023

--- TemperatureDataAnalysis/HSHS4_GLGC_Temps.R
+++ TemperatureDataAnalysis/HSHS4_GLGC_Temps.R
@@ -9,22 +9,36 @@ library(coda)
 library(nleqslv)
 
 fpath <- "/home/ParitoshKRoy/git/ApproximateGLGC/"
-#fpath <- "/home/pkroy/projects/def-aschmidt/pkroy/ApproximateGLGC/" #@ARC
+fpath <- "/home/pkroy/projects/def-aschmidt/pkroy/ApproximateGLGC/" #@ARC
 
 source(paste0(fpath,"Rutilities/utility_functions.R"))
-source(paste0(fpath,"ExactVsApproximateMethods/data_generation.R"))
+load(paste0(fpath,"/TemperatureDataAnalysis/SelectedData/SelectedStatelliteTemps.rda"))
+head(selected.sat.temps)
+table(is.na(selected.sat.temps$MaskTemp))  # FALSE are the locations to be used for modeling
+nsite <- nrow(selected.sat.temps); nsite
 
-# partition as observed and predicted
-obsCoords <- coords[idSampled,]
-prdCoords <- coords[-idSampled,]
-obsY <- y[idSampled]
-prdY <- y[-idSampled]
-obsX <- X[idSampled,]
-prdX <- X[-idSampled,]
-obsZ1 <- z1[idSampled]
-obsZ2 <- z2[idSampled]
-prdZ1 <- z1[-idSampled]
-prdZ2 <- z2[-idSampled]
+####################################################################################
+## Changing the spatial domain
+####################################################################################
+apply(selected.sat.temps[,c("Lon","Lat")], 2, range)  # range of the spatial domain
+selected.sat.temps <- selected.sat.temps %>% 
+  mutate(scaledLon = Lon - mean(range(Lon))) %>%
+  mutate(scaledLat = Lat - mean(range(Lat)))
+apply(selected.sat.temps[,c("scaledLon","scaledLat")], 2, range)
+
+#####################################################################
+# Preparing model objects
+#####################################################################
+X <- selected.sat.temps[,c("scaledLon","scaledLat")]; str(X)
+y <- selected.sat.temps$TrueTemp
+nsize <- selected.sat.temps %>% filter(!is.na(MaskTemp)) %>% nrow(); nsize
+psize <- selected.sat.temps %>% filter(is.na(MaskTemp)) %>% nrow(); psize
+obsY <- selected.sat.temps %>% filter(!is.na(MaskTemp)) %>% .$TrueTemp; str(obsY)
+prdY <- selected.sat.temps %>% filter(is.na(MaskTemp)) %>% .$TrueTemp; str(prdY)
+obsCoords <- selected.sat.temps %>% filter(!is.na(MaskTemp)) %>% select(scaledLon,scaledLat) %>% as.matrix() %>% unname(); str(obsCoords)
+prdCoords <- selected.sat.temps %>% filter(is.na(MaskTemp)) %>% select(scaledLon,scaledLat) %>% as.matrix() %>% unname(); str(prdCoords)
+obsX <- cbind(1,obsCoords); str(obsX)
+prdX <- cbind(1,prdCoords); str(prdX)
 
 obsDistMat <- fields::rdist(obsCoords)
 str(obsDistMat)
@@ -32,60 +46,53 @@ obsDistVec <- obsDistMat[lower.tri(obsDistMat, diag = FALSE)]
 obsMaxDist <- max(obsDistVec)
 obsMedDist <- median(obsDistVec)
 obsMinDist <- min(obsDistVec)
+lLimit <- quantile(obsDistVec, prob = 0.01); lLimit
+uLimit <- quantile(obsDistVec, prob = 0.50); uLimit
 rm(obsDistMat)
-
+quantile(obsDistVec)
 ################################################################################
 # Preparing for Hilbert Space Approximate GP
 ################################################################################
-xRangeDat <- c(-1,1)
-yRangeDat <- c(-1,1)
-Lstar <- c(max(abs(xRangeDat)), max(abs(yRangeDat)))
-
-quantile(obsDistVec, probs = c(1,2.5,52,50)/100)
-min_indentifiable_lscale <- 0.12
-c <- 1.2 + min_indentifiable_lscale; c
-c <- pmax(1.3, 4.5*min_indentifiable_lscale); c # Previously it was c = 1.2
-c <- 1.5
-c <- 1 + 2*0.5; c
-m1 <- round(3.42*c/min_indentifiable_lscale); m1
-m2 <- round(3.42*c/min_indentifiable_lscale); m2
-m1 <- 25
-m2 <- 25
+xyRanges <- apply(selected.sat.temps[,c("scaledLon","scaledLat")], 2, range); xyRanges
+Lstar <- as.numeric(apply(xyRanges, 2, max)); Lstar
+quantile(obsDistVec, probs = seq(1,5,l=2)/100)
+minimum_estimable_lscale <- 0.25*Lstar; minimum_estimable_lscale
+c <- pmax(1.10,4.75*(minimum_estimable_lscale/min(Lstar))); c
+c <- 1.10
+0.10*Lstar
+c <- round(1 + 0.10/Lstar, 2); c
+m1 <- round(3.42*c/(minimum_estimable_lscale/Lstar[1])); m1
+m2 <- round(3.42*c/(minimum_estimable_lscale/Lstar[2])); m2
+m1 <- 40
+m2 <- 40
 mstar <- m1*m2; mstar
-
 L <- c*Lstar; L
 S <- unname(as.matrix(expand.grid(S2 = 1:m1, S1 = 1:m2)[,2:1]))
 str(S)
 lambda <- ((pi*S)/(2*L))^2
 str(lambda)
 head(lambda)
+#############################################################################
+# Prior elicitation
+#############################################################################
 
-## Prior elicitation
-lLimit <- quantile(obsDistVec, prob = 0.01); lLimit
-uLimit <- quantile(obsDistVec, prob = 0.99); uLimit
-
+## Inverse Gamma for length scale
 library(nleqslv)
 ab <- nleqslv(c(5,0.1), getIGamma, lRange = lLimit, uRange = uLimit, prob = 0.98)$x
 ab
 curve(dinvgamma(x, shape = ab[1], scale = ab[2]), 0, uLimit)
-summary(rinvgamma(n = 1000, shape = ab[1], scale = ab[2]))
 
-## Exponential and PC prior
+## Exponential prior for SD
 lambda_sigma1 <- -log(0.01)/1; lambda_sigma1
 lambda_sigma2 <- -log(0.01)/1; lambda_sigma2
 lambda_tau <- -log(0.01)/1; lambda_tau
 pexp(q = 1, rate = lambda_tau, lower.tail = TRUE) ## P(tau > 1) = 0.05
-lambda_ell1 <- as.numeric(-log(0.01)*lLimit); lambda_ell1
-lambda_ell2 <- as.numeric(-log(0.01)*lLimit); lambda_ell2
-pfrechet(q = lLimit, alpha = 1, sigma = lambda_ell2, lower.tail = TRUE) ## P(ell < lLimit) = 0.05
-summary(rfrechet(n = 1000, alpha = 1, sigma = lambda_ell2))
 
-## Stan input
 head(obsX)
 P <- 3
 mu_theta <- c(mean(obsY),rep(0, P-1)); mu_theta
 V_theta <- diag(c(10,rep(1,P-1))); V_theta
-input <- list(N = nsize, M = mstar, P = P, y = obsY, X = obsX, coords = obsCoords, L = L, lambda = lambda, mu_theta = mu_theta, V_theta = V_theta, lambda_sigma1 = lambda_sigma1, lambda_sigma2 = lambda_sigma2, lambda_tau = lambda_tau, a = ab[1], b = ab[2], lambda_ell1 = lambda_ell1, lambda_ell2 = lambda_ell2, positive_skewness = 1)
+input <- list(N = nsize, M = mstar, P = P, y = obsY, X = obsX, coords = obsCoords, L = L, lambda = lambda, mu_theta = mu_theta, V_theta = V_theta, a = ab[1], b = ab[2], lambda_sigma1 = lambda_sigma1, lambda_sigma2 = lambda_sigma2, lambda_tau = lambda_tau, positive_skewness = 0)
 str(input)
 
 library(cmdstanr)
@@ -96,7 +103,7 @@ mod$print()
 cmdstan_fit <- mod$sample(data = input, 
                           chains = 4,
                           parallel_chains = 4,
-                          iter_warmup = 1000,
+                          iter_warmup = 1500,
                           iter_sampling = 1000,
                           adapt_delta = 0.99,
                           max_treedepth = 15,
@@ -108,11 +115,11 @@ elapsed_time$total/3600
 cmdstan_fit$cmdstan_diagnose()
 sampler_diag <- cmdstan_fit$sampler_diagnostics(format = "df")
 str(sampler_diag)
+
 ## Posterior summaries
 pars <- c(paste0("theta[",1:P,"]"),"sigma1","sigma2","ell1","ell2","tau","gamma")
-pars_true_df <- tibble(variable = pars, true = c(theta,sigma1,sigma2,lscale1,lscale2,tau,gamma))
 fit_summary <- cmdstan_fit$summary(NULL, c("mean","sd","quantile50","quantile2.5","quantile97.5","rhat","ess_bulk","ess_tail"))
-fixed_summary <- inner_join(pars_true_df, fit_summary)
+fixed_summary <- fit_summary %>% filter(variable %in% pars)
 fixed_summary %>% print(digits = 3)
 
 ## Posterior draws
@@ -133,14 +140,13 @@ obsH <- t(apply(obsCoords, 1, function(x) eigenfunction_compute(x, L = L, lambda
 str(obsH)
 post_z1 <- t(sapply(1:size_post_samples, function(l) obsH %*% post_omega1[l,])); str(post_z1)
 
-z1_summary <- tibble(z1 = obsZ1,
-                     post.mean = apply(post_z1, 2, mean),
+z1_summary <- tibble(post.mean = apply(post_z1, 2, mean),
                      post.sd = apply(post_z1, 2, sd),
                      post.q2.5 = apply(post_z1, 2, quantile2.5),
                      post.q50 = apply(post_z1, 2, quantile50),
                      post.q97.5 = apply(post_z1, 2, quantile97.5))
 z1_summary
-z1_summary %>% mutate(btw = between(z1, post.q2.5,post.q97.5)) %>% .$btw %>% mean()
+
 
 ## Recovery of random effect z2
 size_post_samples <- nrow(draws_df); size_post_samples
@@ -149,14 +155,12 @@ obsH <- t(apply(obsCoords, 1, function(x) eigenfunction_compute(x, L = L, lambda
 str(obsH)
 post_z2 <- t(sapply(1:size_post_samples, function(l) obsH %*% post_omega2[l,])); str(post_z2)
 
-z2_summary <- tibble(z2 = obsZ2,
-                     post.mean = apply(post_z2, 2, mean),
+z2_summary <- tibble(post.mean = apply(post_z2, 2, mean),
                      post.sd = apply(post_z2, 2, sd),
                      post.q2.5 = apply(post_z2, 2, quantile2.5),
                      post.q50 = apply(post_z2, 2, quantile50),
                      post.q97.5 = apply(post_z2, 2, quantile97.5))
 z2_summary
-z2_summary %>% mutate(btw = between(z2, post.q2.5,post.q97.5)) %>% .$btw %>% mean()
 
 ## Recovery of z <- gamma*exp(z1) + z2
 size_post_samples <- nrow(draws_df); size_post_samples
@@ -165,34 +169,29 @@ str(post_z1)
 str(post_z2)
 l <- 1
 post_z <- t(sapply(1:size_post_samples, function(l) post_gamma[l]*exp(post_z1[l,]) + post_z2[l,])); str(post_z)
-z <- gamma*exp(z1) + z2
-z_summary <- tibble(z = z[idSampled],
-                    post.mean = apply(post_z, 2, mean),
+z_summary <- tibble(post.mean = apply(post_z, 2, mean),
                     post.sd = apply(post_z, 2, sd),
                     post.q2.5 = apply(post_z, 2, quantile2.5),
                     post.q50 = apply(post_z, 2, quantile50),
                     post.q97.5 = apply(post_z, 2, quantile97.5))
 z_summary
-save(elapsed_time, fixed_summary, draws_df, z1_summary, z2_summary, z_summary, file = paste0(fpath,"ExactVsApproximateMethods/HSHS_GLGC.RData"))
+save(elapsed_time, fixed_summary, draws_df, z1_summary, z2_summary, z_summary, file = paste0(fpath,"TemperatureDataAnalysis/HSHS4_GLGC_Temps.RData"))
 
 ##################################################################
 ## Independent prediction at each predictions sites
 ##################################################################
-
-### Random effect z1 at predicted locations
+## Random effect z1 at predicted locations
 psize <- nrow(prdCoords); psize
 predH <- t(apply(prdCoords, 1, function(x) eigenfunction_compute(x, L = L, lambda = lambda))); str(predH)
 post_z1pred <- t(sapply(1:size_post_samples, function(l) predH %*% post_omega1[l,])); str(post_z1pred)
 
 z1pred_summary <- tibble(
-  z1 = prdZ1,
   post.mean = apply(post_z1pred, 2, mean),
   post.sd = apply(post_z1pred, 2, sd),
   post.q2.5 = apply(post_z1pred, 2, quantile2.5),
   post.q50 = apply(post_z1pred, 2, quantile50),
   post.q97.5 = apply(post_z1pred, 2, quantile97.5))
-head(z1pred_summary)
-mean(z1pred_summary[,"z1"] > z1pred_summary[,"post.q2.5"] & z1pred_summary[,"z1"] < z1pred_summary[,"post.q97.5"])
+z1pred_summary
 
 ### Random effect z2 at predicted locations
 psize <- nrow(prdCoords); psize
@@ -200,14 +199,12 @@ predH <- t(apply(prdCoords, 1, function(x) eigenfunction_compute(x, L = L, lambd
 post_z2pred <- t(sapply(1:size_post_samples, function(l) predH %*% post_omega2[l,])); str(post_z2pred)
 
 z2pred_summary <- tibble(
-  z2 = prdZ2,
   post.mean = apply(post_z2pred, 2, mean),
   post.sd = apply(post_z2pred, 2, sd),
   post.q2.5 = apply(post_z2pred, 2, quantile2.5),
   post.q50 = apply(post_z2pred, 2, quantile50),
   post.q97.5 = apply(post_z2pred, 2, quantile97.5))
-head(z2pred_summary)
-mean(z2pred_summary[,"z2"] > z2pred_summary[,"post.q2.5"] & z2pred_summary[,"z2"] < z2pred_summary[,"post.q97.5"])
+z2pred_summary
 
 ## Compute the means
 post_theta <- as_tibble(draws_df) %>% select(starts_with("theta[")) %>% as.matrix() %>% unname(); str(post_theta)
@@ -220,6 +217,7 @@ str(post_theta)
 
 obsXtheta <- t(sapply(1:size_post_samples, function(l) obsX %*% post_theta[l,])); str(obsXtheta)
 prdXtheta <- t(sapply(1:size_post_samples, function(l) prdX %*% post_theta[l,])); str(prdXtheta)
+
 post_tau <- as_tibble(draws_df) %>% .$tau; str(post_tau)
 post_gamma <- as_tibble(draws_df) %>% .$gamma; str(post_gamma)
 str(post_z1pred)
@@ -234,26 +232,26 @@ pred_summary <- tibble(
   post.q50 = apply(ypred_draws, 2, quantile50),
   post.q97.5 = apply(ypred_draws, 2, quantile97.5),
   y = prdY)
-head(pred_summary)
-mean(pred_summary[,"y"]>pred_summary[,"post.q2.5"] & pred_summary[,"y"]<pred_summary[,"post.q97.5"])
+pred_summary
 
 ## Computation for scoring rules
-
+## In the object PrdY there are 160 observations that are missing, to compute the scoring rules we ignore these cases
+id_full_missing <- which(is.na(prdY)); str(id_full_missing)
 library(scoringRules)
-ES <- es_sample(y = prdY, dat = t(ypred_draws)); ES
-#VS0.25 <- vs_sample(y = prdY, dat = t(ypred_draws), p = 0.25); VS0.25
-logs <- mean(logs_sample(y = prdY, dat = t(ypred_draws))); logs
-CRPS <- mean(crps_sample(y = prdY, dat = t(ypred_draws))); CRPS
+ES <- es_sample(y = prdY[-id_full_missing], dat = t(ypred_draws)[-id_full_missing,]); ES
+#VS0.25 <- vs_sample(y = prdY[-id_full_missing], dat = t(ypred_draws)[-id_full_missing,], p = 0.25); VS0.25
+logs <- mean(logs_sample(y = prdY[-id_full_missing], dat = t(ypred_draws)[-id_full_missing,])); logs
+CRPS <- mean(crps_sample(y = prdY[-id_full_missing], dat = t(ypred_draws)[-id_full_missing,])); CRPS
 
-scores_df <- pred_summary %>% 
+scores_df <- pred_summary %>% filter(!is.na(y)) %>%
   mutate(intervals = scoringutils::interval_score(true_values = y, lower = post.q2.5, upper = post.q50, interval_range = 0.95)) %>%
   mutate(btw = between(y,post.q2.5, post.q97.5)) %>%
   mutate(error = y - post.q50) %>%
   summarise(MAE = sqrt(mean(abs(error))), RMSE = sqrt(mean(error^2)), CVG = mean(btw),
             IS = mean(intervals)) %>%
-  mutate(ES = ES, logs = logs, CRPS = CRPS,  `Elapsed Time` = elapsed_time$total, Method = "HSHS_GLGC") %>%
+  mutate(ES = ES, logs = logs, CRPS = CRPS,  `Elapsed Time` = elapsed_time$total, Method = "HSHS4_GLGC") %>%
   select(Method,MAE,RMSE,CVG,CRPS,IS,ES,logs,`Elapsed Time`)
 scores_df
 
-save(elapsed_time, fixed_summary, draws_df, z1_summary, z2_summary, z_summary, post_z, pred_summary, scores_df, file = paste0(fpath,"ExactVsApproximateMethods/HSHS_GLGC.RData"))
+save(m1,m2,mstar,elapsed_time, fixed_summary, draws_df, z1_summary, z2_summary, z_summary, pred_summary, scores_df, file = paste0(fpath,"TemperatureDataAnalysis/HSHS4_GLGC_Temps.RData"))
 
