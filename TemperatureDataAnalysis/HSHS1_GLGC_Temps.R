86cb7210b1e2ad511f70ac596f5dcce926fbdd40
Paritosh Kumar Roy <paritosh.roy@mail.mcgill.ca>
2023-12-06 10:46:58 -0500

    Commit on 6 December 2023

--- TemperatureDataAnalysis/HSHS1_GLGC_Temps.R
+++ TemperatureDataAnalysis/HSHS1_GLGC_Temps.R
@@ -55,7 +55,7 @@ quantile(obsDistVec)
 ################################################################################
 xyRanges <- apply(selected.sat.temps[,c("scaledLon","scaledLat")], 2, range); xyRanges
 Lstar <- as.numeric(apply(xyRanges, 2, max)); Lstar
-ell_hat <- 0.04;
+ell_hat <- 0.03;
 ell_hat/Lstar
 c <- pmax(1.20,4.75*(ell_hat/min(Lstar))); c
 m1 <- round(3.42*c/(ell_hat/Lstar[1])); m1
