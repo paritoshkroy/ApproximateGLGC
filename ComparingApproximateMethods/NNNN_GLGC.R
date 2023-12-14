00905505fcd7bb26e33959ef3d37de046efc20f3
Paritosh Kumar Roy <paritosh.roy@mail.mcgill.ca>
2023-12-14 10:13:31 -0500

    Commit on 14 December 2023

--- ComparingApproximateMethods/NNNN_GLGC.R
+++ ComparingApproximateMethods/NNNN_GLGC.R
@@ -106,7 +106,7 @@ mod$print()
 cmdstan_fit <- mod$sample(data = input, 
                           chains = 4,
                           parallel_chains = 4,
-                          iter_warmup = 1500,
+                          iter_warmup = 1000,
                           iter_sampling = 1000,
                           adapt_delta = 0.99,
                           max_treedepth = 15,
