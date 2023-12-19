fe03d465604c261761aa16c08b1aaacd8c5b4b3d
Paritosh Kumar Roy <paritosh.roy@mail.mcgill.ca>
2023-12-19 03:26:38 -0500

    Commit on 19 December 2023

--- ComparingApproximateMethods/HSHS_GLGC.sh
+++ ComparingApproximateMethods/HSHS_GLGC.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=301-310  # 50 nodes runs this model independently
+#SBATCH --array=311-350  # 50 nodes runs this model independently
 #SBATCH --account=def-aschmidt  # replace this with your own account
 #SBATCH --ntasks=16              # number of processes
 #SBATCH --mem-per-cpu=16000M      # memory; default unit is megabytes
